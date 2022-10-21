ebab64391607cbd6fd8185d46dcab19a6eb7f875
bapata <bapata@ymail.com>
2022-10-21 11:56:29 -0700

    Update target_sum_indices.go

--- target_sum_indices.go
+++ target_sum_indices.go
@@ -1,31 +1,43 @@
 package main
 
+// Given input_list=[1,4,6,5],target=10
+// returns: [1,2] (because 4(index=1)+6(index=2) = 10 (target))
+
 import (
-  "fmt"
+	"fmt"
 )
 
 type MyTargetLists struct {
-  input_list []int
-  target int
-  target_index_list []int
+	input_list        []int
+	target            int
+	target_index_list []int
 }
 
-func (tl *MyTargetLists )set_target(t int) {
-  tl.target = t
+func (tl *MyTargetLists) set_target(t int) {
+	tl.target = t
 }
 
-func (tl *MyTargetLists )set_input_list(mylist []int) {
-  tl.input_list= mylist
+func (tl *MyTargetLists) set_input_list(mylist []int) {
+	tl.input_list = mylist
 }
 
 // algorithm
-func (tl *MyTargetLists )get_index_list() []int {
-  return tl.target_index_list
+func (tl *MyTargetLists) get_index_list() []int {
+	for ii := 0; ii < len(tl.input_list); ii++ {
+		for jj := 0; jj < len(tl.input_list); jj++ {
+			if tl.input_list[ii]+tl.input_list[jj] == tl.target {
+				tl.target_index_list = append(tl.target_index_list, ii)
+				tl.target_index_list = append(tl.target_index_list, jj)
+				return tl.target_index_list
+			}
+		}
+	}
+	return tl.target_index_list
 }
 
 func main() {
-  mtl := MyTargetLists{}
-  mtl.set_target(10)
-  mtl.set_input_list([]int{10,20})
-  fmt.Println(mtl)
+	mtl := MyTargetLists{}
+	mtl.set_target(3)
+	mtl.set_input_list([]int{10, 20, 1, 2, 45, 7})
+	fmt.Println(mtl.get_index_list())
 }
